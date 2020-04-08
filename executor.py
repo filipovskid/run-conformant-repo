c2ee1cecb9570cfd7891b236466e48fe5b925480
Darko Filipovski <darkofilipovski0@gmail.com>
2020-04-08 21:24:18 +0200

    Using environment variables

--- executor.py
+++ executor.py
@@ -4,9 +4,16 @@ from drboson.drboson import DRBoson
 from drboson.DRBosonProducer import RemoteProducer
 import socket
 import run
+import os
 
 
 def main():
+
+    bootstrap_servers = os.environ.get('BOOTSTRAP_SERVERS')
+    producer_topic = os.environ.get('PRODUCER_TOPIC')
+    run_id = os.environ.get("RUN_ID")
+    project_id = os.environ.get("PROJECT_ID")
+
     conf = {
         'bootstrap.servers': '192.168.1.4',
         'client.id': socket.gethostname(),
