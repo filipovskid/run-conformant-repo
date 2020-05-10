415dcd8140da3e933258e31e68242ec0792fee0c
Darko Filipovski <darkofilipovski0@gmail.com>
2020-05-10 23:43:22 +0200

    Fixing Run dataset directory path

--- drboson/drboson.py
+++ drboson/drboson.py
@@ -60,7 +60,7 @@ class Run(object):
         self.id = run_id
         self.project_id = project_id
         self.work_dir = pathlib.Path(work_dir)
-        self.dataset_dir = dataset_dir
+        self.dataset_dir = pathlib.Path(dataset_dir)
 
         if self.work_dir.is_dir() is False:
             raise NotADirectoryError('drboson: Work directory is supposed to be a directory')
